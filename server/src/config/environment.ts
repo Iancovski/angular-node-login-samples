bd1ddbef92aa2519a2ef27bc3be1412c32eb57f1
Iancovski <46848500+Iancovski@users.noreply.github.com>
2024-01-21 02:53:07 -0300

    Update environment.ts

--- server/src/config/environment.ts
+++ server/src/config/environment.ts
@@ -2,7 +2,7 @@ const environment = {
     production: process.env.NODE_ENV === 'production',
     port: process.env.PORT || 3000,
     google: {
-        clientId: '352111847351-q974jpoc2nsvg9d7phnipir4olnnijib.apps.googleusercontent.com'
+        clientId: process.env.GOOGLE_CLIENT_ID
     }
 };
 
