b5d602a3ba2f3a2561faf3f96214d1ed13737c47
Iancovski <46848500+Iancovski@users.noreply.github.com>
2024-03-12 23:17:25 -0300

    Login Button Components
    Created standalone components for the social login buttons.

---
+++ client/src/app/pages/login/components/facebook-login-button/facebook-login-button.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FacebookLoginButtonComponent } from './facebook-login-button.component';
+
+describe('FacebookLoginButtonComponent', () => {
+  let component: FacebookLoginButtonComponent;
+  let fixture: ComponentFixture<FacebookLoginButtonComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [FacebookLoginButtonComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(FacebookLoginButtonComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
